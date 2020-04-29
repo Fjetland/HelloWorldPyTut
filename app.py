e6424546262e9b086f31fd453063362eeaba8a8b
Andreas <andreas.fjetland@gmail.com>
2020-04-30 00:12:41 +0200

    update

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-i = 1
-while i <=5:
-    print('*'*i)
-    i+=1
-print("Done")
+secretNumber = 9
+guessCount = 0
+maxGuesses = 5
+while guessCount < maxGuesses:
+    guess = int(input("Guess a number: "))
+    guessCount+=1
+    if secretNumber == guess:
+        print("Yey you won!")
+        break
+    else:
+        print("Looser, try again!")
+        if guess < secretNumber:
+            print("Guess higher")
+        else:
+            print("Guess lower")
+else:
+    print("You lost the simple game!")
+print("Awsome game has exited the building")
