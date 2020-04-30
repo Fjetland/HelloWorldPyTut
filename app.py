e6b7823be175843c0c3e904606d99c21f7e64541
Andreas <andreas.fjetland@gmail.com>
2020-04-30 19:54:41 +0200

    update

--- app.py
+++ app.py
@@ -1,27 +1,4 @@
-is_running = False
-inn = ""
-while True:
-    inn = input(">").upper()
-    if inn == "HELP":
-        print('''
-start - to start the car
-stop - to stop the car
-quit - to exit
-        ''')
-    elif inn == "START":
-        if is_running:
-            print("Car is already running.")
-        else:
-            is_running = True
-            print("Car started... Ready to go!")
-    elif inn == "STOP":
-        if not is_running:
-            print("Car is not on.")
-        else:
-            is_running = False
-            print("Car stopped.")
-    elif inn == "quit":
-        break
-    else:
-        print("I don't understand that")
-print("Bye Bye")
+num = [5,2,5,2,2]
+
+for x in num:
+    print('x'*x)
