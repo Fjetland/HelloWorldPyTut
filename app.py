845ce4ea7add570bdec973246b08c2bf16e36666
Andreas <andreas.fjetland@gmail.com>
2020-05-01 07:55:37 +0200

    update

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-customer = {"name": "John Smith", "age": 30, "is_verified": True, "hest": "Ku"}
-
-print(customer.get("hest"))
-
-# Task
-phone = input("Phone: ")
-
-numWords = {
-    "1": "One ",
-    "2": "Two ",
-    "3": "Three ",
-    "4": "Four ",
-    "5": "Five ",
-    "6": "Six ",
-    "7": "Seven ",
-    "8": "Eight ",
-    "9": "Nine ",
-    "0": "Zero"
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)": "😊️️",
+    ":(": "☹️"
 }
 
-wordList = ""
-for digit in phone:
-    wordList += numWords.get(digit)
-print(wordList)
+out = ""
+for word in words:
+     out += emojis.get(word, word) + " "
+
+print(out)
