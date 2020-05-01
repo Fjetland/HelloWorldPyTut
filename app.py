54556901b8fc1345c387834ceaed2576f5b0f28f
Andreas <andreas.fjetland@gmail.com>
2020-05-01 11:42:47 +0200

    update

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import converters
 from converters import kg_to_lbs
+from utils import find_max
 
 print(converters.lbs_to_kg(2))
 print(kg_to_lbs(2))
+
+# Max list test
+my_list = [1, 4, 8, 90, 2, 4, 1, 7, 12]
+print(find_max(my_list))
