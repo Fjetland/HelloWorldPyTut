d4bc00b71f9f2bedf436aff385ee9af94a924e5e
Andreas <andreas.fjetland@gmail.com>
2020-05-01 11:32:12 +0200

    update

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-class Dog:
+class Mammal:
     def walk(self):
         print("Walk")
+
+
+class Dog(Mammal):
+    def bark(self):
+        print("Woof")
+
+
+class Cat(Mammal):
+    def be_annoying(self):
+        print("Annoying")
+
+
+dog = Dog()
+dog.walk()
+dog.bark()
