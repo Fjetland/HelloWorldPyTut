caa8e7106506612d1a5e03b459818ba58e542b96
Andreas <andreas.fjetland@gmail.com>
2020-05-01 11:38:55 +0200

    update

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
-class Mammal:
-    def walk(self):
-        print("Walk")
+import converters
+from converters import kg_to_lbs
 
-
-class Dog(Mammal):
-    def bark(self):
-        print("Woof")
-
-
-class Cat(Mammal):
-    def be_annoying(self):
-        print("Annoying")
-
-
-dog = Dog()
-dog.walk()
-dog.bark()
+print(converters.lbs_to_kg(2))
+print(kg_to_lbs(2))
