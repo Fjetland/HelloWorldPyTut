7145e2d5c2f6389f8164bac3cad8fdb1ab90888a
Andreas <andreas.fjetland@gmail.com>
2020-05-01 07:43:08 +0200

    update

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-numbers = (1, 2, 3)
-x = numbers[0]
-y = numbers[1]
-z = numbers[2]
+customer = {"name": "John Smith", "age": 30, "is_verified": True, "hest": "Ku"}
 
-xy, yy, zy = numbers
+print(customer.get("hest"))
+
+# Task
+phone = input("Phone: ")
+
+numWords = {
+    "1": "One ",
+    "2": "Two ",
+    "3": "Three ",
+    "4": "Four ",
+    "5": "Five ",
+    "6": "Six ",
+    "7": "Seven ",
+    "8": "Eight ",
+    "9": "Nine ",
+    "0": "Zero"
+}
+
+wordList = ""
+for digit in phone:
+    wordList += numWords.get(digit)
+print(wordList)
